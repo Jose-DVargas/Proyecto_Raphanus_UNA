c0750aab544b512f9dd25d66ce292eddcc0ff29e
JoseDVargas <josedanielvargas@uned.cr>
2021-06-18 02:01:45 -0600

    Corrección de data

--- Scripts/ANOVA Luz.R
+++ Scripts/ANOVA Luz.R
@@ -8,6 +8,7 @@ cat("\014")     # Remove script in console
 #Import the data set
 #------------------------------------------------
 library(readr)
-data <- read.table("https://github.com/Jose-DVargas/Proyecto_Raphanus_UNA/blob/main/Data/luz.txt", header = TRUE, dec = ".",sep = "")
+data <- read.table("https://github.com/Jose-DVargas/Proyecto_Raphanus_UNA/blob/main/Data/luz.txt", header = TRUE, dec = ".", sep = "")
 
-?read.table()
+data <- read.table(file = "Data/luz.txt", header = TRUE, dec = ".", sep = "")
+data
