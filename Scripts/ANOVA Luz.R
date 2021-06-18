f2b6e6daf2c97f53143785f5857e36e0beea881a
JoseDVargas <josedanielvargas@uned.cr>
2021-06-18 02:58:06 -0600

    Fix

--- Scripts/ANOVA Luz.R
+++ Scripts/ANOVA Luz.R
@@ -6,15 +6,10 @@ graphics.off()  # Remove all graphics
 cat("\014")     # Remove script in console
 options(digits = 3)
 set.seed(2021) 
-
 #------------------------------------------------
-#Import the data set
 #libraries
 #------------------------------------------------
 library(readr)
-data <- read.table("https://github.com/Jose-DVargas/Proyecto_Raphanus_UNA/blob/main/Data/luz.txt", header = TRUE, dec = ".", sep = "")
-
-data <- read.table(file = "Data/luz.txt", header = TRUE, dec = ".", sep = "")
 library(ggplot2)
 #------------------------------------------------
 #Import the data set
@@ -32,11 +27,6 @@ ds$luz <- as.factor(ds$luz)
 anova <- aov(formula = peso ~ luz + luz/Distancias, data = ds)
 summary.lm(anova)
 TukeyHSD(anova)
-
-
-plot <- ggplot(data = ds,aes(x = luz, y = peso, fill=Distancias)) + geom_bar(stat="identity", alpha=0.8)+ scale_fill_brewer(palette="PRGn")
-plot
-
 
 
 anova2 <- aov(formula = hojas ~ luz + luz/Distancias, data = ds)
